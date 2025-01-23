5de983d2a92eccb0083b675555d5e089efb69614
skozub <sebastiankozub@o2.pl>
2025-01-23 21:01:55 +0100

    ffmpeg & yt-dlp - both works on image

--- UtubeRest/Controllers/ValuesController.cs
+++ UtubeRest/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,7 +13,30 @@ namespace UtubeRest.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            var ffMpegVersionCommmand = "ffmpeg -version";
+            var ytDlpVersionCommand = "yt-dlp --version";
+
+            var ffmepegVersion = RunUnixCommand(ffMpegVersionCommmand);
+            var ytDlpVersion = RunUnixCommand(ytDlpVersionCommand);
+
+            return new string[] { ffmepegVersion, ytDlpVersion };
+        }
+
+        private string RunUnixCommand(string command)
+        {
+            var process = new Process();
+            process.StartInfo.FileName = "/bin/bash";
+            process.StartInfo.Arguments = $"-c \"{command}\"";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+
+            process.Start();
+
+            var output = process.StandardOutput.ReadToEnd();
+            var error = process.StandardError.ReadToEnd();
+
+            return output;
         }
 
         // GET api/<ValuesController>/5
